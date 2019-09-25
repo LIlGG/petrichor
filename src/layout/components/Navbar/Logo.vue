12e06aa312a1c3a0ea2333573fcda89f872a3cdb
lxy <1103069291@qq.com>
2019-09-25 20:54:22 +0800

    修改样式

--- src/layout/components/Navbar/Logo.vue
+++ src/layout/components/Navbar/Logo.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="site-branding">
-    <span class="site-title">
-      <span class="logolink moe-mashiro">
-        <a :href="logo.url" :alt="logo.title">
-          <ruby>
-            <span class="sakuraso">{{logo.sakuraso}}</span>
-            <span class="no">{{logo.no}}</span>
-            <span class="shironeko">{{logo.shironeko}}</span>
-            <rp></rp>
-            <rt class="chinese-font">{{logo.title}}</rt>
-            <rp></rp>
-          </ruby>
-        </a>
-      </span>
+  <span class="site-title">
+    <span class="logolink moe-mashiro">
+      <a :href="logo.url" :alt="logo.title">
+        <ruby>
+          <span class="sakuraso">{{logo.sakuraso}}</span>
+          <span class="no">{{logo.no}}</span>
+          <span class="shironeko">{{logo.shironeko}}</span>
+          <rp></rp>
+          <rt class="chinese-font">{{logo.title}}</rt>
+          <rp></rp>
+        </ruby>
+      </a>
     </span>
-  </div>
+  </span>
 </template>
 
 <script>
@@ -28,146 +26,130 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
-.site-branding {
-  animation: sitetop 1s;
-  float: left;
-  position: relative;
-  height: 75px;
-  line-height: 75px;
-  margin-left: -6px;
-  .site-title {
-    margin: 0;
-    a {
-      color: #464646;
-      font-weight: 400;
-      &:hover {
-        color: orange !important;
-      }
+.site-title {
+  margin: 0;
+  a {
+    color: #464646;
+    font-weight: 400;
+    &:hover {
+      color: orange !important;
     }
+  }
 
-    img {
-      height: 40px;
-      margin-top: 17px;
-      opacity: 0.75;
-      -webkit-transition: color 0.2s ease-out, border 0.2s ease-out,
-        opacity 0.2s ease-out;
-      -moz-transition: color 0.2s ease-out, border 0.2s ease-out,
-        opacity 0.2s ease-out;
-      transition: color 0.2s ease-out, border 0.2s ease-out,
-        opacity 0.2s ease-out;
-      &:hover {
-        opacity: 1;
-      }
+  img {
+    height: 40px;
+    margin-top: 17px;
+    opacity: 0.75;
+    -webkit-transition: color 0.2s ease-out, border 0.2s ease-out,
+      opacity 0.2s ease-out;
+    -moz-transition: color 0.2s ease-out, border 0.2s ease-out,
+      opacity 0.2s ease-out;
+    transition: color 0.2s ease-out, border 0.2s ease-out, opacity 0.2s ease-out;
+    &:hover {
+      opacity: 1;
     }
+  }
 
-    .logolink {
-      &.moe-mashiro {
-        a {
-          color: #464646;
-          float: left;
-          font-size: 25px;
-          font-weight: 800;
-          height: 56px;
-          line-height: 56px;
-          padding-left: 6px;
-          padding-right: 15px;
-          padding-top: 11px;
-          text-decoration-line: none;
-        }
-
-        .sakuraso,
-        .no {
-          font-size: 25px;
-          border-radius: 9px;
-          padding-bottom: 2px;
-          padding-top: 5px;
-        }
-
-        .no {
-          display: inline-block;
-          margin-left: 5px;
-        }
-      }
-
+  .logolink {
+    &.moe-mashiro {
       a {
         color: #464646;
         float: left;
-        font-size: 20px;
+        font-size: 25px;
         font-weight: 800;
         height: 56px;
         line-height: 56px;
-        padding-left: 35px;
+        padding-left: 6px;
         padding-right: 15px;
         padding-top: 11px;
         text-decoration-line: none;
-        &:hover {
-          .sakuraso {
-            background-color: orange;
-            color: #fff;
-          }
+      }
 
-          .shironeko,
-          rt {
-            color: orange;
-          }
+      .sakuraso,
+      .no {
+        font-size: 25px;
+        border-radius: 9px;
+        padding-bottom: 2px;
+        padding-top: 5px;
+      }
 
-          .no {
-            -webkit-animation: spin 1.5s linear infinite;
-            animation: spin 1.5s linear infinite;
-          }
+      .no {
+        display: inline-block;
+        margin-left: 5px;
+      }
+    }
 
-          ruby {
-            rt {
-              opacity: 1;
-            }
-          }
+    a {
+      color: #464646;
+      float: left;
+      font-size: 20px;
+      font-weight: 800;
+      height: 56px;
+      line-height: 56px;
+      padding-left: 35px;
+      padding-right: 15px;
+      padding-top: 11px;
+      text-decoration-line: none;
+      &:hover {
+        .sakuraso {
+          background-color: orange;
+          color: #fff;
         }
-      }
 
-      .sakuraso {
-        background-color: rgba(255, 255, 255, 0.5);
-        border-radius: 5px;
-        color: #464646;
-        height: auto;
-        line-height: 25px;
-        margin-right: 0;
-        padding-bottom: 0;
-        padding-top: 1px;
-        text-size-adjust: 100%;
-        width: auto;
-      }
-
-      ruby {
-        ruby-position: under;
-        -webkit-ruby-position: after;
+        .shironeko,
         rt {
-          font-size: 10px;
-          transform: translateY(-15px);
-          opacity: 0;
-          transition-property: opacity;
-          transition-duration: 0.5s, 0.5s;
+          color: orange;
         }
+
+        .no {
+          -webkit-animation: spin 1.5s linear infinite;
+          animation: spin 1.5s linear infinite;
+        }
+
+        ruby {
+          rt {
+            opacity: 1;
+          }
+        }
+      }
+    }
+
+    .sakuraso {
+      background-color: rgba(255, 255, 255, 0.5);
+      border-radius: 5px;
+      color: #464646;
+      height: auto;
+      line-height: 25px;
+      margin-right: 0;
+      padding-bottom: 0;
+      padding-top: 1px;
+      text-size-adjust: 100%;
+      width: auto;
+    }
+
+    ruby {
+      ruby-position: under;
+      -webkit-ruby-position: after;
+      rt {
+        font-size: 10px;
+        transform: translateY(-15px);
+        opacity: 0;
+        transition-property: opacity;
+        transition-duration: 0.5s, 0.5s;
       }
     }
   }
 }
 
 @media (max-width: 860px) {
-  .site-branding {
-    float: right;
-    height: 50px;
-    line-height: 43px;
-    margin-right: -6px;
-    .site-title {
-      font-size: 20px;
-      margin-right: 10px;
-      img {
-        height: 40px;
-        line-height: 40px;
-        margin: 0;
-        vertical-align: middle;
-      }
+  .site-title {
+    font-size: 20px;
+    margin-right: 10px;
+    img {
+      height: 40px;
+      line-height: 40px;
+      margin: 0;
+      vertical-align: middle;
     }
   }
 }
--- src/layout/components/Navbar/index.vue
+++ src/layout/components/Navbar/index.vue
@@ -2,17 +2,24 @@
     petrichor 通用组件 - 导航栏
  -->
 <template>
-    <header
-        class="site-header no-select is-homepage gizle"
-        :class="[{sabit: isTop}, {yya: !isTop}]"
-    >
+  <header class="site-header no-select is-homepage gizle" :class="[{sabit: isTop}, {yya: !isTop}]">
+    <div class="site-top">
+      <div class="site-branding">
         <logo :logo="logoInfo"></logo>
+      </div>
+      <div class="lower-cantiner">
         <menu>
-            <nav-item></nav-item>
+          <nav-item></nav-item>
         </menu>
-        <div></div>
-        <div></div>
-    </header>
+      </div>
+      <div class="header-user-avatar">
+        <login></login>
+      </div>
+      <div class="searchbox">
+        <search></search>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -34,10 +41,10 @@ export default {
       let scrollY = this.$store.getters.scrollY;
       if (scrollY != 0) {
         return false;
-      } 
+      }
       return true;
     }
-  },
+  }
 };
 </script>
 
@@ -90,5 +97,145 @@ export default {
 .sabit {
   top: 0;
   z-index: 9999;
+}
+
+.site-top {
+  width: 100%;
+  display: block;
+  margin: 0 auto;
+  padding: 0 20px;
+  .lower-cantiner {
+    position: absolute;
+    left: 50%;
+    min-width: 758.4px;
+    pointer-events: none;
+  }
+  .lower {
+    display: inline-block;
+    margin: 15px 0 0 10px;
+    font-size: 16px;
+    position: relative;
+    left: -50%;
+    pointer-events: auto !important;
+    nav {
+      position: relative;
+      float: right;
+      display: none;
+      animation: searchbox 0.2s;
+      &.navbar {
+        display: block;
+      }
+    }
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: inline-block;
+    li {
+      float: left;
+      margin: 0 13px;
+      position: relative;
+      -webkit-transtition: all 1s ease;
+      a {
+        padding: 10px 0;
+        display: inline-block;
+        color: #666;
+        &:hover {
+          color: orange;
+          &:after {
+            max-width: 100%;
+          }
+        }
+        &:after {
+          content: "";
+          display: block;
+          position: absolute;
+          bottom: -16px;
+          height: 6px;
+          background-color: #fe9600;
+          width: 100%;
+          max-width: 0;
+          transition: max-width 0.25s ease-in-out;
+        }
+      }
+
+      ul {
+        li {
+          a {
+            &:hover {
+              content: "";
+              display: none;
+              position: absolute;
+              bottom: -21px;
+              height: 6px;
+              background-color: #fe9600;
+              width: 100%;
+              max-width: 0;
+              transition: max-width 0.25s ease-in-out;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .menu-item-has-children {
+    li {
+      a {
+        font-size: 14px;
+        padding: 8px 18px;
+        text-transform: none;
+        color: #545454;
+      }
+    }
+  }
+}
+
+.site-branding {
+  animation: sitetop 1s;
+  float: left;
+  position: relative;
+  height: 75px;
+  line-height: 75px;
+  margin-left: -6px;
+}
+
+@media (max-width: 1200px) {
+  .site-top .lower {
+    left: 15px;
+    .lower-cantiner {
+      left: 0;
+    }
+    nav {
+      &.hide {
+        display: none !important;
+      }
+    }
+  }
+}
+
+@media (max-width: 860px) {
+  .site-top {
+    .lower {
+      nav {
+        ul {
+          li {
+            .animated-hover {
+              i {
+                display: inline;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .site-branding {
+    float: right;
+    height: 50px;
+    line-height: 43px;
+    margin-right: -6px;
+  }
 }
 </style>
