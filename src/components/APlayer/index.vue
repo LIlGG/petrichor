cb1ad260520951809719b16746b0a860dcb51c49
lxy <1103069291@qq.com>
2019-09-29 18:45:42 +0800

    使用组件方式集成Aplayer和live2d

---
+++ src/components/APlayer/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div ref="aplayer"></div>
+</template>
+
+<script>
+import "aplayer/dist/APlayer.min.css";
+import APlayer from "aplayer";
+export default {
+  name:"APlayer",
+  props: ["options"],
+  data() {
+      return {
+        ap: ""
+    }
+  },
+  watch: {
+    options(newValue, oldValue) {
+      if(this.options.audio) {
+        this.options.container= this.$refs.aplayer
+        this.ap = new APlayer(this.options);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
+
