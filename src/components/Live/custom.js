25c709eff8b6e1518ac51939b2417b9cd29806c0
lxy <1103069291@qq.com>
2019-10-21 19:32:35 +0800

    live2d增加新的选择器

--- src/components/Live/custom.js
+++ src/components/Live/custom.js
@@ -1,28 +1,72 @@
 export default {
-    'mouseover': [{
-      'selector': ['.live2d'],
-      'message': ['干嘛呢你，快把手拿开', '鼠…鼠标放错地方了吧！', '别过来,变态!']
-    }, {
-      'selector': ['#Home'],
-      'message': ['想看我的主页么?']
-    }, {
-      'selector': ['#Change'],
-      'message': ['变装！']
-    }, {
-      'selector': ['#Save'],
-      'message': ['保持微笑就好~']
-    }, {
-      'selector': ['#About'],
-      'message': ['想知道我的一切?']
-    }, {
-      'selector': ['#Hide'],
-      'message': ['哼! 你会后悔的!!']
-    }],
-    'click': [
-      {
-        'selector': ['.live2d'],
-        'message': ['是…是不小心碰到了吧', '萝莉控是什么呀', '你看到我的小熊了吗?', '再摸的话我可要报警了！⌇●﹏●⌇', '110吗，这里有个变态一直在摸我(ó﹏ò｡)', '我要生气哟(ー`´ー)']
-      }
-    ]
+  'mouseover': [{
+    'selector': ['.live2d'],
+    'message': ['干嘛呢你，快把手拿开', '鼠…鼠标放错地方了吧！', '别过来,变态!']
+  }, {
+    'selector': ['#Home'],
+    'message': ['想看我的主页么?']
+  }, {
+    'selector': ['#Change'],
+    'message': ['变装！']
+  }, {
+    'selector': ['#Save'],
+    'message': ['保持微笑就好~']
+  }, {
+    'selector': ['#About'],
+    'message': ['想知道我的一切?']
+  }, {
+    'selector': ['#Hide'],
+    'message': ['哼! 你会后悔的!!']
+  },{
+    'selector': ['.aplayer-play'],
+    'message': ['播放']
+  }, {
+    'selector': ['.aplayer-pause'],
+    'message': ['暂停']
+  },{
+    'selector': ['#video-btn'],
+    'message': ['想播放视频吗？可以先关闭左下角背景音乐~~']
+  },{
+    'selector': ['#video-btn'],
+    'message': ['想播放视频吗？可以先关闭左下角背景音乐~~']
+  },{
+    'selector': ['.changeSkin-gear'],
+    'message': ['可以换皮肤哦~']
+  },{
+    'selector': ['.cd-top'],
+    'message': ['回到最开始的地方吧~']
+  },{
+    'selector': ['#white-bg'],
+    'message': ['默认是纯洁的白色~~']
+  },{
+    'selector': ['#sakura-bg'],
+    'message': ['樱花飞舞~']
+  },{
+    'selector': ['#gribs-bg'],
+    'message': ['格子控！']
+  },{
+    'selector': ['#KAdots-bg'],
+    'message': ['小点点~']
+  },{
+    'selector': ['#totem-bg'],
+    'message': ['奇迹的橙色！']
+  },{
+    'selector': ['#pixiv-bg'],
+    'message': ['Pixiv主题~']
+  },{
+    'selector': ['#bing-bg'],
+    'message': ['必应每日图片的说~~']
+  },{
+    'selector': ['#dark-bg'],
+    'message': ['关灯！']
   }
-  
+],
+  'click': [
+    {
+      'selector': ['.live2d'],
+      'message': ['是…是不小心碰到了吧', '萝莉控是什么呀', '你看到我的小熊了吗?', '再摸的话我可要报警了！⌇●﹏●⌇', '110吗，这里有个变态一直在摸我(ó﹏ò｡)', '我要生气哟(ー`´ー)']
+    }
+  ]
+}
+
+
