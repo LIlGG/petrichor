745d8c6a4159938f984c58c8e7f85edcdb15e29d
lxy <1103069291@qq.com>
2019-10-28 20:53:34 +0800

    增加首页PAGE第一部分

--- src/page/page.vue
+++ src/page/page.vue
@@ -4,13 +4,16 @@
 当前界面只实现最基本的功能，如翻页，展示等，具体功能视后台html而定
  -->
 <template>
-    <div id="page" class="site wrapper">
-        <div class="blank" style="padding-top: 75px;"></div>
-        <div>
-            <!--<component :is="componentDiv"></component> -->
-            <!-- html模板可以从后台传过来，也可以预先写好模板, 目前来说最好是先写一个本地模板，之后全部采用后端读取 -->
-        </div>
-        <!-- <div class="pattern-center">
+  <div id="page" class="site wrapper">
+    <div class="blank" style="padding-top: 0px;"></div>
+    <div id="content" class="site-content">
+      <feature :feature="feature"/>
+    </div>
+    <!-- <div> -->
+    <!--<component :is="componentDiv"></component> -->
+    <!-- html模板可以从后台传过来，也可以预先写好模板, 目前来说最好是先写一个本地模板，之后全部采用后端读取 -->
+    <!-- </div> -->
+    <!-- <div class="pattern-center">
             <div class="pattern-attachment-img">
                
             </div>
@@ -20,12 +23,16 @@
         </div>
         <div id="content" class="site-content">
             
-        </div>-->
-    </div>
+    </div>-->
+  </div>
 </template>
 
 <script>
+import Feature from "@/views/Feature";
 export default {
+  components: {
+    Feature
+  },
   data() {
     return {
       shtml: `<div><aplayer :musics="{ server: 'netease', type: 'playlist',id: 2345868969 }" :option="{fixed: false}"></aplayer><aplayer :musics="{ server: 'netease', type: 'playlist',id: 2345868969 }" :option="{fixed: false}"></aplayer></div>`,
@@ -35,14 +42,59 @@ export default {
         id: 2345868969
       },
       musicOption: {
-          fixed: false
+        fixed: false
+      },
+      feature: {
+        title: " START:DASH!!",
+        featureList: [
+          {
+            link: "/theme-sakura/",
+            img: "https://cdn.jsdelivr.net/gh/moezx/cdn@3.3.2/img/other/sakura.md.png",
+            title: "Sakura",
+            description: "本站 WordPress 主题"
+          },
+          {
+            link: "/theme-sakura/",
+            img: "https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.6/img/other/th%20(3).jpg",
+            title: "Pixiv",
+            description: "P 站的正确打开方式"
+          },
+          {
+            link: "/theme-sakura/",
+            img: "https://view.moezx.cc/images/2019/06/11/74751807_angel.jpg",
+            title: "明日方舟",
+            description: "游戏主界面 H5 复刻"
+          }
+        ]
       }
     };
   },
   computed: {
     componentDiv() {
-      return {template: this.shtml};
+      return { template: this.shtml };
     }
   }
 };
 </script>
+<style lang="less">
+.wrapper {
+  animation: fade-in;
+  animation-duration: 0.5s;
+}
+.site-content {
+  max-width: 800px;
+  padding: 0 10px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: rgba(255, 255, 255, 0.8);
+  &::after {
+    content: "";
+    display: table;
+    table-layout: fixed;
+    clear: both
+  }
+}
+#content {
+    animation: main 1s
+}
+</style>
---
+++ src/views/Feature/index.vue
@@ -0,0 +1,149 @@
+<template>
+    <div class="top-feature-row">
+        <h1 class="fes-title" style="font-family: 'Ubuntu', sans-serif;">
+            <i class="fa fa-anchor" aria-hidden="true"></i>
+            {{feature.title}}
+        </h1>
+        <div
+            class="top-feature-v2"
+            v-for="(item,index) in feature.featureList"
+            :key="index"
+            :style="featureStyle(index)"
+        >
+            <div class="the-feature square from_left_and_right">
+                <a :href="item.link" target="_blank">
+                    <div class="img">
+                        <img :src="item.img">
+                    </div>
+                    <div class="info">
+                        <h3>{{item.title}}</h3>
+                        <p>{{item.description}}</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "feature",
+  props: {
+    feature: {
+      type: Object,
+      default: {}
+    }
+  },
+  methods: {
+    featureStyle(index) {
+      if (index == 1) {
+        return "margin: 0 4.5px;";
+      }
+      return "";
+    }
+  }
+};
+</script>
+<style lang="less">
+.top-feature-row {
+  width: 100%;
+  height: auto;
+  margin-top: 55px;
+}
+h1.fes-title,
+h1.main-title {
+  color: #666;
+  font-size: 16px;
+  font-weight: 400;
+  padding-bottom: 5px;
+  margin-bottom: 30px;
+  border-bottom: 1px dashed #ececec;
+}
+.top-feature-v2 {
+  display: inline-block;
+}
+.the-feature,
+.the-feature * {
+  box-sizing: border-box;
+}
+.the-feature {
+  position: relative;
+  transition: all 0.35s ease-in-out;
+
+  a {
+    display: block;
+    color: #333;
+    &:hover {
+      text-decoration: none;
+    }
+  }
+  img {
+    height: 160px;
+    width: 257px;
+  }
+}
+.the-feature.from_left_and_right {
+  position: relative;
+  height: 160px;
+  width: 253px;
+  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.3);
+  overflow: hidden;
+  border-radius: 10px;
+  .img {
+    transition: all 0.35s ease-in-out;
+    transform: scale(1);
+  }
+  .info {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    text-align: center;
+    backface-visibility: hidden;
+    background: #333;
+    background: rgba(0, 0, 0, 0.6);
+    visibility: hidden;
+    opacity: 0;
+    transition: all 0.35s ease-in-out;
+    h3 {
+      text-transform: uppercase;
+      color: #fff;
+      text-align: center;
+      font-size: 17px;
+      padding: 10px;
+      background: #111;
+      margin: 30px 0 0 0;
+      transition: all 0.35s ease-in-out;
+      transform: translateX(-100%);
+    }
+    p {
+      font-style: italic;
+      font-size: 12px;
+      position: relative;
+      color: #bbb;
+      padding: 20px 20px 20px;
+      text-align: center;
+      transition: all 0.35s 0.1s linear;
+      transform: translateX(100%);
+    }
+  }
+
+  a {
+    &:hover {
+      .img {
+        transform: scale(1.2);
+      }
+      .info {
+        visibility: visible;
+        opacity: 1;
+        h3,
+        p {
+          transform: translateX(0);
+        }
+      }
+    }
+  }
+}
+</style>
+
